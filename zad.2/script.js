a5aaff69ae07cf6fe93b21760443134b5bdb969b
Barbara Banach <b_code@MacBook-Air-Barbara.local>
2016-11-21 23:27:42 +0100

    new tasks

---
+++ zad.2/script.js
@@ -0,0 +1,20 @@
+function Button(text) {
+        this.text = text || 'Hello';
+}
+
+Button.prototype = {
+    create: function() {
+            var self = this;
+            this.$element = $('<button>');
+            this.$element.text(this.text);
+            $('body').append(this.$element);
+            this.$element.click(function() {
+                 alert(self.text);
+            
+            });
+    }
+}
+
+var btn1 = new Button('HEY HEY');
+btn1.create();
+
